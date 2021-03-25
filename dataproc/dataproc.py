da537037f3c8c5b8399a12c43cff92fa1238d21a
Will Thompson <will.k.t@gmail.com>
2021-03-25 10:37:31 -0500

    updates

--- dataproc/dataproc.py
+++ dataproc/dataproc.py
@@ -2,6 +2,7 @@
 import os
 import tempfile
 import zipfile
+import typer
 from pathlib import Path
 
 
@@ -12,5 +13,9 @@ def unzip_files(in_dir: Path, out_dir: Path) -> None:
     :return:
     """
     for zip_file in in_dir.glob("*.zip"):
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            zip_ref.extractall(out_dir)
+        typer.secho(f"Unzipping: {zip_file.name}", bg=typer.colors.BLACK, fg=typer.colors.BRIGHT_YELLOW)
+        try:
+            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                zip_ref.extractall(out_dir)
+        except Exception as e:
+            typer.secho("failed!", bg=typer.colors.BLACK, fg=typer.colors.BRIGHT_MAGENTA)
